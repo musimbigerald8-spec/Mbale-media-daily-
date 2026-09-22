fe742f21aeeb9bd2fb6dff0107c54787d23bdc5c
Mbale media daily <musimbigerald8@gmail.com>
2026-09-22 19:30:30 +0300

    Add offline app shell

---
+++ radio-app/sw.js
@@ -0,0 +1,1 @@
+const CACHE='mbale-radio-v1';self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.webmanifest']))));self.addEventListener('fetch',e=>{if(e.request.method==='GET')e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))})
